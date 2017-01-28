37b61c8ce1412030378b023ba53bfa1cdca23338
Sholpan Kruger <sholpanmkt@gmail.com>
2017-01-28 12:30:44 -0500

    test github rover

--- rover.js
+++ rover.js
@@ -1,25 +1,119 @@
+//my rover
 var myRover = {
-  position: [0,0], 
-  direction: 'N'
+position: [0, 0],
+direction: 'N',
+marsGrid: [
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
 };
 
-function goForward(rover) {
-  switch(rover.direction) {
-    case 'N':
-      rover.position[0]++
-      break;
-    case 'E':
-      rover.position[1]++
-      break;
-    case 'S':
-      rover.position[0]--
-      break;
-    case 'W':
-      rover.position[1]--
-      break;
-  };
-
-  console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]")
+function preventSuddenDeath() {
+if (myRover.position[0] > 9) {
+myRover.position[0] = 0;
+}
+if (myRover.position[0] < 0) {
+myRover.position[0] = 9;
+}
+if (myRover.position[1] > 9) {
+myRover.position[1] = 0;
+}
+if (myRover.position[1] < 0) {
+myRover.position[1] = 9;
+}
 }
 
-goForward(myRover);
+
+//change rover position
+function newPosition(move) {
+
+//move rover forward
+if (move === 'f') {
+switch (myRover.direction) {
+  case 'N':
+    myRover.position[0] -= 1;
+    break;
+  case 'E':
+    myRover.position[1] += 1;
+    break;
+  case 'S':
+    myRover.position[0] += 1;
+    break;
+  case 'W':
+    myRover.position[1] -= 1;
+    break;
+}}
+
+//move rover backward
+if (move === 'b') {
+  switch (myRover.direction) {
+    case 'N':
+      myRover.position[0] += 1;
+      break;
+    case 'E':
+      myRover.position[1] -= 1;
+      break;
+    case 'S':
+      myRover.position[0] -= 1;
+      break;
+    case 'W':
+      myRover.position[1] += 1;
+      break;
+  }}
+
+  preventSuddenDeath();
+  return myRover.position;
+ }
+
+
+//change rover direction
+function newDirection(turn) {
+
+//turn rover left
+if (turn === 'l') {
+  switch (myRover.direction) {
+    case 'N':
+      myRover.direction = 'W';
+      break;
+    case 'E':
+      myRover.direction = 'N';
+      break;
+    case 'S':
+      myRover.direction = 'E';
+      break;
+    case 'W':
+      myRover.direction = 'S';
+      break;
+  }}
+
+//turn rover right
+if (turn === 'r') {
+  switch (myRover.direction) {
+      case 'N':
+      myRover.direction = 'E';
+      break;
+    case 'E':
+      myRover.direction = 'S';
+      break;
+    case 'S':
+      myRover.direction = 'W';
+      break;
+    case 'W':
+      myRover.direction = 'N';
+      break;
+}}
+  return myRover.direction;
+}
+
+function userDirections() {
+var input = document.getElementById("userInput");
+alert(input);
+}
